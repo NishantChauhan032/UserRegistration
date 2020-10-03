6bc1b572701c56f5f25cf9b0ad6b62da21fc0281
NishantChauhan032 <nishantkr0210@gmail.com>
2020-10-03 23:24:54 +0530

    [NISHANT] add: Mobile Number Validated

--- UserRegistration.java
+++ UserRegistration.java
@@ -10,7 +10,7 @@ public class UserRegistration{
           while(!Pattern.matches("[A-Z]{1}[a-z]{2,}",firstName)){
              System.out.println("Invalid Input. Please enter a correct input for first name: ");
              firstName =  sc.next();
-     }
+          }
 
      System.out.println("Enter the Last Name: ");
      String lastName = sc.next();
@@ -18,14 +18,22 @@ public class UserRegistration{
           while(!Pattern.matches("[A-Z]{1}[a-z]{2,}",lastName)){
              System.out.println("Invalid Input. Please enter a correct input for last name: ");
              lastName =  sc.next();
-     }
+          }
      
      System.out.println("Enter the Email ID: ");
      String emailId = sc.next();
        
           while(!Pattern.matches("^[A-Za-z0-9]+(\\.[A-Za-z0-9-]+)?@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)?(\\.[A-Za-z]{2,})$",emailId)){
              System.out.println("Invalid Input. Please enter a correct input for Email ID: ");
-             lastName =  sc.next();
-     }
+             emailId =  sc.next();
+          }
+      sc.nextLine();      
+      System.out.println("Enter the Mobile Number: ");
+      String mobileNo = sc.nextLine();
+
+          	while(!Pattern.matches("[0-9]{2}[ ][0-9]{10}",mobileNo)){
+             System.out.println("Invalid Input. Please enter a correct input for Mobile Number: ");
+             mobileNo =  sc.nextLine();
+          }
    }
  }
