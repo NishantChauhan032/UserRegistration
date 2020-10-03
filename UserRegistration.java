76bc5fe644bacba43c9656898362a295cbffbe15
NishantChauhan032 <nishantkr0210@gmail.com>
2020-10-04 00:55:55 +0530

    [NISHANT] refactor: Special Character checked in the password

--- UserRegistration.java
+++ UserRegistration.java
@@ -39,7 +39,7 @@ public class UserRegistration{
       System.out.println("Enter the password: ");
       String password = sc.next();
 
-          while(!Pattern.matches("^(?=.*[A-Z])(?=.*[0-9]).{8,}$",password)){
+          while(!Pattern.matches("(?=^.{8,}$)(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]*[@#$%_][0-9a-zA-Z]*",password)){
              System.out.println("Invalid Input. Please enter a correct input for password: ");
              password =  sc.next();
           }
